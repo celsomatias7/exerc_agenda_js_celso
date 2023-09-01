bf1e91bab2bab7b8c998561e3aa10eb5b96cc3d7
Celso Matias <celsomatias7@gmail.com>
2023-09-01 11:42:42 -0300

    projeto agenda de contatos

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 const form = document.getElementById('form-atividade'); /*criação da variável FORM */
-/*Colocando os emojis de reação a situação de aprovado ou não */
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji triste" />';
+
 /*criação de 2 arrays para armazenar as notas e atividades do usuário */
 const atividades = []; 
 const notas = [];
-/*criação do span para colorir a ação de Aprovado ou Reprovado*/
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-
-const notaMinima = parseFloat(prompt("Digite uma nota mínima:"));
 
 /*adicionando uma regra para não sobrepor valores na tabela e sim adicionar*/
 let linhas = '';
@@ -19,55 +12,36 @@ form.addEventListener('submit',function(e) { /*regra de validação do submit do
 
     adicionaLinha();
     atualizaTabela();
-    atualizaMediaFinal();
-
+    
 });
 
 function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumTelefone = document.getElementById('num-telefone');
 
     /*para não permitir que o usuário coloque a mesma atividade duas vezes*/
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade ${inputNomeAtividade.value} já foi inserida`);
+    if (atividades.includes(inputNomeContato.value)) {
+        alert(`O nome ${inputNomeContato.value} já foi inserido`);
     } else {
         /*adicionando um push que force a gravação dos dados dos usuários */
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
+    atividades.push(inputNomeContato.value);
+    notas.push(parseFloat(inputNumTelefone.value));
 
     /*adicionando as informação de Nome e nota no corpo da tabela */
     let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`; /*condicional se foi reprovado ou não ?=Verdadeiro :=Falso */
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumTelefone.value}</td>`;
     linha += `</tr>`;
 
     /*adicionando uma regra para não sobrepor valores na tabela e sim adicionar*/
     linhas += linha;
     }    
 
-    inputNomeAtividade.value='';
-    inputNotaAtividade.value='';
+    inputNomeContato.value='';
+    inputNumTelefone.value='';
 }
 
 function atualizaTabela () {
     const corpoTabela = document.querySelector('tbody'); /*preenchendo a tabela com os dados*/
     corpoTabela.innerHTML = linhas;
 }
-
-function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2); /*tofixed serve para limitar as casas decimais */
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;    
-    }
-
-    function calculaMediaFinal() {
-        let somaDasNotas = 0;
-
-        for (let i=0; i < notas.length; i++) { /*primeiro para fazer a média das notas é somar todas as notas inseridas pelo user */
-            somaDasNotas += notas[i];
-        }
-
-        return somaDasNotas / notas.length; /*segundo passo dividir a soma acima pelo numero de notas inseridas, contadas pelo LENGTH */
-    }
